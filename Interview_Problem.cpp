2e118b6a59b7140ab8dbc82db84f875d32d19c0e
pratap-raghuvir <115841178+pratap-raghuvir@users.noreply.github.com>
2022-10-15 02:38:31 +0530

    Error reported
    Program showing lot of memory is used and not giving correct answer.

---
+++ Interview_Problem.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  //Taking the number as input
+  int a;
+  cin>>a;
+  //Printing the multiples of 5 less than a
+  int num =1;
+  while(num<=x)
+  {
+    if((num%5)==0)
+    {
+      cout<<num<<endl;
+    }
+    
+  
+   }
+  return 0;
+}
